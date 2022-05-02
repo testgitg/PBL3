3232a377e4d000e4b15363599210420ee0903f61
mkhoatd <binnhatle@gmail.com>
2022-05-02 07:31:17 +0700

    test

---
+++ ECom.DataAccess/Data/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ECom.DataAccess.Data;
+
+public class ApplicationDbContextFactory:IDesignTimeDbContextFactory<ApplicationDbContext>
+
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        optionsBuilder.UseSqlServer("Server=.;Database=EComWeb;Trusted_Connection=True");
+
+        return new ApplicationDbContext(optionsBuilder.Options);
+
+    }
+}
